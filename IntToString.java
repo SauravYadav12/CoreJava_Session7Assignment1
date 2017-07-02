b4b3a2bee0969d163f9b0a6055885556ac109604
SauravYadav12 <saurav1286@gmail.com>
2017-07-02 09:30:47 +0530

    Add files via upload

---
+++ IntToString.java
@@ -0,0 +1,15 @@
+class IntToString {
+
+public static void main(String[] args) {
+
+int i = 10;
+
+// Integer to String conversion using inbuilt String class toString;
+
+String str1 = Integer.toString(i);
+
+System.out.println("This is the conversion from Integer to String " + str1);
+
+   }
+
+}
